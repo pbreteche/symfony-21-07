56042ba782dc80e02ad7331ecfd7464292afe165
Pierre Bretéché <pbreteche@dawan.fr>
2021-07-05 14:46:06 +0200

    Objets Request et Response

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends AbstractController
@@ -23,8 +24,14 @@ class HelloController extends AbstractController
         return new Response('Bonjour '.$name);
     }
 
-    public function numbered(int $id): Response
+    public function numbered(int $id, Request $request): Response
     {
+        $version = $request->query->get('version', 'latest'); // $_GET['version']
+        if ('POST' === $request->getMethod()) {
+            $postData = $request->request->get('data'); //$_POST['data'];
+            $request->getSession()->set('post', $postData); // $_SESSION
+        }
+
         return new Response($id);
     }
 }
