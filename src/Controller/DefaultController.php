a6bfe8b89b3cf130c429375b973682fd108b7285
Pierre Bretéché <pbreteche@dawan.fr>
2021-07-06 15:46:12 +0200

    classe de formulaire

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -45,17 +45,7 @@ class DefaultController extends AbstractController
     ): Response {
         $article = new Article();
 
-        $form = $this->createFormBuilder($article)
-            ->add('title')
-            ->add('body')
-            ->add('publishedAt', DateTimeType::class, [
-                'date_widget' => 'single_text',
-                'time_widget' => 'single_text',
-                'input'  => 'datetime_immutable',
-                'required' => false,
-            ])
-            ->getForm()
-        ;
+        $form = $this->createForm(ArticleType::class, $article);
 
         $form->handleRequest($request);
 
@@ -84,19 +74,9 @@ class DefaultController extends AbstractController
         Request $request,
         EntityManagerInterface $manager
     ): Response {
-        $form = $this->createFormBuilder($article, [
+        $form = $this->createForm(ArticleType::class, $article, [
             'method' => 'PUT',
-        ])
-            ->add('title')
-            ->add('body')
-            ->add('publishedAt', DateTimeType::class, [
-                'date_widget' => 'single_text',
-                'time_widget' => 'single_text',
-                'input'  => 'datetime_immutable',
-                'required' => false,
-            ])
-            ->getForm()
-        ;
+        ]);
 
         $form->handleRequest($request);
 
