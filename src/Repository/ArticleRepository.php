5d6ec6198bf8571cc264162c0de5c36f302639e5
Pierre Bretéché <pbreteche@dawan.fr>
2021-07-07 13:45:37 +0200

    présentation DQL

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -19,15 +19,31 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-     /**
-      * @return Article[] Returns an array of Article objects
-      */
+    /**
+    * @return Article[] Returns an array of Article objects
+    */
     public function findByMonth(\DateTimeImmutable $month)
     {
         return $this->createQueryBuilder('article')
             ->andWhere('article.publishedAt >= :first_day')
             ->andWhere('article.publishedAt < :last_day')
             ->getQuery()
+            ->setParameters([
+                'first_day' => $month->modify('first day of this month midnight'),
+                'last_day' => $month->modify('first day of next month midnight'),
+            ])->getResult();
+    }
+
+    /**
+     * @return Article[] Returns an array of Article objects
+     */
+    public function findByMonth2(\DateTimeImmutable $month)
+    {
+        return $this->getEntityManager()->createQuery(
+            'SELECT a, author FROM '.Article::class.' a'.
+            ' INNER JOIN a.writtenBy author'.
+            ' WHERE a.publishedAt >= :first_day AND a.publishedAt < :last_day'
+        )
             ->setParameters([
                 'first_day' => $month->modify('first day of this month midnight'),
                 'last_day' => $month->modify('first day of next month midnight'),
