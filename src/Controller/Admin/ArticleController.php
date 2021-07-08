bbfd1473970c29c56f1a6f93dc07b7a0cec36b2d
Pierre Bretéché <pbreteche@dawan.fr>
2021-07-08 13:46:34 +0200

    présentation différents emplacement de contrôle d'accès

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,11 +19,22 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/new", methods={"GET", "POST"})
+     * @IsGranted("ROLE_ADMIN")
      */
     public function new(
         Request $request,
         EntityManagerInterface $manager
     ): Response {
+
+        // gestion du contrôle d'accès personnalisée
+        $user = $this->getUser();
+        if (!$user || !$this->isGranted('ROLE_ADMIN', $user)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        // ou version raccourcie
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $article = (new Article())
             ->setCreatedAt(new \DateTimeImmutable())
         ;
