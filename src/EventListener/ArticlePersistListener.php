dc1f5cf534c6a1bda0e98f741b84b9ca147bed0d
Pierre Bretéché <pbreteche@dawan.fr>
2021-07-09 11:39:56 +0200

    Exemple d'envoi de mail

--- src/EventListener/ArticlePersistListener.php
+++ src/EventListener/ArticlePersistListener.php
@@ -4,22 +4,44 @@ namespace App\EventListener;
 
 use App\Entity\Article;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
-use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ArticlePersistListener
 {
     /**
-     * @var \Psr\Log\LoggerInterface
+     * @var MailerInterface
      */
-    private $logger;
+    private $mailer;
+    /**
+     * @var \Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface
+     */
+    private $flashBag;
 
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
+    public function __construct(
+        MailerInterface $mailer,
+        FlashBagInterface $flashBag
+    ) {
+        $this->mailer = $mailer;
+        $this->flashBag = $flashBag;
     }
 
     public function postPersist(Article $article, LifecycleEventArgs $eventArgs)
     {
-        $this->logger->info('Nouvel article : '.$article->getTitle());
+        $email = (new Email())
+            ->to('recipient@example.com')
+            ->from('sender@example.com')
+            ->subject('Un nouvel article a été créé')
+            ->text('le contenu au format texte')
+            ->html('<p>Le contenu au format HTML</p>')
+        ;
+
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $exception) {
+            $this->flashBag->add('error', 'L\'envoi de l\'email a échoué');
+        }
     }
 }
